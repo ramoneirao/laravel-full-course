caa05533638daba75cd0e47a925730d3cda4b681
ramoneirao <88005095+ramoneirao@users.noreply.github.com>
2025-07-08 00:25:20 -0300

    feat: update about route and add debug information

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $pessoa = [
+        "nome" => "Ramon",
+        "email" => "ramon@email.com ",
+    ];
+
+    dd($pessoa);
+
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::view('/about', 'about');
+
+Route::get('/product/{id}', function(string $id) {
+    return "Works!  {$id}";
+})->whereNumber('id');
